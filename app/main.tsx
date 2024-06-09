187a25405b693a695dac94c6a816ea1542f5a44e
jhollyfer <jhollyfer.fr@gmail.com>
2024-06-09 17:11:04 -0500

    form example

--- app/main.tsx
+++ app/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './globals.css';
 import { Root } from './root';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- app/root.tsx
+++ app/root.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 
 import { Button } from '@components/ui/button';
 import { Input } from '@components/ui/input';
-import './globals.css';
+import { CheckCircle2Icon } from 'lucide-react';
 
 export function Root(): React.ReactElement {
 	return (
 		<div className="h-screen w-screen flex items-center justify-center">
 			<form
-				className="p-8 flex gap-2"
+				className="p-8 flex gap-2 max-w-2xl w-full"
 				onSubmit={(e) => e.preventDefault()}
 			>
-				<Input placeholder="Full Name" />
-				<Button variant="default">Submit</Button>
+				<Input
+					placeholder="Full Name"
+					className="flex-1rounded-md border border-green-600 placeholder:text-green-400"
+				/>
+				<Button
+					variant="default"
+					className="bg-green-600"
+				>
+					<CheckCircle2Icon />
+				</Button>
 			</form>
 		</div>
 	);
