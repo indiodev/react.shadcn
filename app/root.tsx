1458f3753491dfcaa9396b05cbaccdb94ccb15a5
jhollyfer <jhollyfer.fr@gmail.com>
2024-06-09 17:07:23 -0500

    added button and input

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import { Button } from '@components/ui/button';
+import { Input } from '@components/ui/input';
 import './globals.css';
 
 export function Root(): React.ReactElement {
 	return (
-		<div>
-			<h1>Vite + React</h1>
+		<div className="h-screen w-screen flex items-center justify-center">
+			<form
+				className="p-8 flex gap-2"
+				onSubmit={(e) => e.preventDefault()}
+			>
+				<Input placeholder="Full Name" />
+				<Button variant="default">Submit</Button>
+			</form>
 		</div>
 	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			Components: resolve(__dirname, './app/components'),
+			'@components': resolve(__dirname, './app/components'),
+			'@libs': resolve(__dirname, './app/libs'),
 		},
 	},
 });
