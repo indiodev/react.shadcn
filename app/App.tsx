671d9c4402d2d2bc7fd24255a049a5f53ae289ba
jhollyfer <jhollyfer.fr@gmail.com>
2024-06-09 16:50:35 -0500

    configure shadcn

--- app/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function App(): React.ReactElement {
-  return (
-    <div>
-      <h1>Vite + React</h1>
-    </div>
-  );
-}
---
+++ app/root.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './globals.css';
+
+export function Root(): React.ReactElement {
+	return (
+		<div>
+			<h1>Vite + React</h1>
+		</div>
+	);
+}
